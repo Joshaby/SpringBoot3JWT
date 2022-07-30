9dc69460d65cf4e5d88b86f8f8371df1ab654fe7
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-07-30 01:32:58 -0300

    Primeiro commit

---
+++ src/main/java/com/joshaby/springboot3jwt/Springboot3jwtApplication.java
@@ -0,0 +1,13 @@
+package com.joshaby.springboot3jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3jwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3jwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshaby/springboot3jwt/Springboot3jwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshaby.springboot3jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3jwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
